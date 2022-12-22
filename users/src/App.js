86800acc10fb9e0dfe69f286261fb3d67a6ea0e6
yuval01958@gmail.com <yuval01958@gmail.com>
2022-12-22 17:57:18 +0200

    20_12_lesson

--- users/src/App.js
+++ users/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import { usersData } from './json';
+import { useState } from 'react';
 
 function App() {
+
+  const [addName, setName] = useState('')
+  const [NamesList,setNameList] = useState([])
+
+  function handleToDoChange(e){
+    setName(e.target.value)
+  }
+
+  function addingName(){
+    setNameList([...NamesList,addName])
+    console.log(NamesList)
+  }
 
 
   return (
     <div className="App">
       <h3>all names and emails:</h3>
+      <input value = {addName} onChange = {handleToDoChange}></input> 
+      <button onClick={addingName}>add to list</button>
       <table>
             <tr>
               <th>name</th>
@@ -23,6 +38,17 @@ function App() {
             })}
 
           </table>
+          <h3>adding names</h3>
+          <ul>
+            {NamesList.map(
+              addTheName => (
+                <li>{addTheName} </li>
+              )
+            )
+
+            }
+          </ul>
+
     </div>
   );
 };

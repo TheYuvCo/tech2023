c4e271d068bf9ffe899daf1131e59dd4611d13d2
yuval01958@gmail.com <yuval01958@gmail.com>
2023-03-09 21:54:21 +0200

    update

---
+++ crowfunding/src/Components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavMenu, Nav, NavLink, Logo } from "./NavbarElements";
+import LogoImg from "../../assests/LOGO2.png"
+
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<NavMenu>
+            <Logo src ={LogoImg}></Logo>
+		<NavLink to="/Home" activeStyle>
+			Home
+		</NavLink>
+		<NavLink to="/CreateCampaign" activeStyle>
+            Create Campaign
+		</NavLink>
+		</NavMenu>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
